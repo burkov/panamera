02a53f4c73d6baa83a2d5e0dd484fb0b7c38daa2
alexander.burkov <alexander.burkov@jetbrains.com>
2020-12-26 02:05:53 +0300

    Pin footer down

--- src/components/page/index.tsx
+++ src/components/page/index.tsx
@@ -1,23 +1,27 @@
 import React, { FC } from 'react';
 import { Container, Hr, LayoutContent, LayoutFooter, LayoutHeader } from '@webteam/layout';
 import GitInfo from 'react-git-info/macro';
-import dayjs from "dayjs";
+import dayjs from 'dayjs';
+import styles from './page.module.css';
 
 export const Page: FC<{}> = ({ children }) => {
   const {
     branch,
-    tags,
     commit: { date, shortHash },
   } = GitInfo();
   return (
-    <Container>
-      <LayoutHeader>
-        <h1 className="wt-h1">Panamera: a Porsche for your env params</h1>
-      </LayoutHeader>
-      <LayoutContent>{children}</LayoutContent>
-      <LayoutFooter>
+    <Container className={styles.pageContainer}>
+      <div className={styles.contentWrapper}>
+        <LayoutHeader>
+          <h1 className="wt-h1">Panamera: a Porsche for your env params</h1>
+        </LayoutHeader>
+        <LayoutContent className="wt-offset-top-24">{children}</LayoutContent>
+      </div>
+      <LayoutFooter className={styles.footer}>
         <Hr />
-        <p className="wt-text-3">Build {dayjs(date).fromNow()} </p>
+        <p className="wt-text-3">
+          commit {shortHash} | <span className={styles.branch}>{branch}</span> | built {dayjs(date).fromNow()}
+        </p>
       </LayoutFooter>
     </Container>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { App } from './components/app';
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
+import 'reset-css'
 
 dayjs.extend(relativeTime);
 
