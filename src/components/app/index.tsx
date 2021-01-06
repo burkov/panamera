28e94fe82579c1419bf5618af245d375e202a2a5
alexander.burkov <alexander.burkov@jetbrains.com>
2021-01-06 19:19:26 +0300

    Use react-router for switching between credentials prompt and main page

--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -3,8 +3,25 @@ import '@webteam/typography';
 import { useAwsCredentialsStorage } from '../../core/use-aws-credentials-storage';
 import { CredentialsPrompt } from '../creds-prompt';
 import { ParamsTable } from '../params-table';
+import { Route, Switch, useHistory } from 'react-router-dom';
+import { RouteWithCredentials } from '../redirect-if-missing';
 
 export const App: FC = () => {
   const [credentials, setCredentials, removeCredentials] = useAwsCredentialsStorage();
-  return credentials === undefined ? <CredentialsPrompt onCredentialsSelected={setCredentials} /> : <ParamsTable credentials={credentials} removeCredentials={removeCredentials} />;
+  const history = useHistory();
+  return (
+    <Switch>
+      <Route path="/credentials">
+        <CredentialsPrompt
+          onCredentialsSelected={(credentials) => {
+            setCredentials(credentials);
+            history.replace('/');
+          }}
+        />
+      </Route>
+      <RouteWithCredentials credentials={credentials} redirectTo="/credentials" path="/" exact={true}>
+        <ParamsTable credentials={credentials!} removeCredentials={removeCredentials} />
+      </RouteWithCredentials>
+    </Switch>
+  );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -6,12 +6,15 @@ import 'reset-css';
 import './styles/restore.css';
 import '@webteam/colors/lib/index.css';
 import { App } from './components/app';
+import { HashRouter } from 'react-router-dom';
 
 dayjs.extend(relativeTime);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root'),
 );
