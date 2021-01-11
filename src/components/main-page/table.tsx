c36133689ac5d02d9744bd186f9ede57d8ae57bd
alexander.burkov <alexander.burkov@jetbrains.com>
2021-01-11 21:04:52 +0300

    Add fuzzy searching

--- src/components/main-page/table.tsx
+++ src/components/main-page/table.tsx
@@ -1,8 +1,12 @@
-import React, { FC, useMemo } from 'react';
+import React, { FC, useMemo, useState } from 'react';
 import { ParameterWithPrefix } from '../../core/use-aws-parameters-storage';
 import styles from './params-table.module.css';
 import _ from 'lodash';
 import { COLOR_SUCCESS, COLOR_WARNING } from '@webteam/colors';
+import Input from '@webteam/input';
+import { Col, Row } from '@webteam/layout';
+import Fuse from 'fuse.js';
+import Highlighter from 'react-highlight-words';
 
 const prefixes = ['dev1', 'dev2', 'dev3', 'dev4', 'dev5', 'audt', 'stgn/blue', 'stgn/green'];
 
@@ -30,8 +34,7 @@ export const PrefixRow: FC<{ paramsForName: ParameterWithPrefix[] }> = ({ params
                 ? forGivenPrefix
                     .map(({ Value, Prefix }, _, list) =>
                       list.length > 1
-                        ? `${Prefix
-                            .slice(Prefix.lastIndexOf('/') + 1)
+                        ? `${Prefix.slice(Prefix.lastIndexOf('/') + 1)
                             .replace('lservice', 'ls')
                             .replace('allapps', 'all')}: ${truncate(32, Value!)}`
                         : truncate(32, Value!),
@@ -47,38 +50,53 @@ export const PrefixRow: FC<{ paramsForName: ParameterWithPrefix[] }> = ({ params
 };
 
 export const ParametersTable: FC<{ params: ParameterWithPrefix[] }> = ({ params }) => {
-  const map = useMemo(() => {
-    const ps = new Set<string>();
+  const [search, setSearch] = useState('');
+  const sortedParams = useMemo(() => {
     const r = new Map<string, ParameterWithPrefix[]>();
     params.forEach((p) => {
-      ps.add(p.Prefix);
       const name = p.Name!.slice(p.Prefix.length);
       const old = r.get(name) ?? [];
       r.set(name, [...old, p]);
     });
-    return Array.from(r.entries()).sort();
+    return Array.from(r.entries())
+      .sort()
+      .map(([name, paramsForName]) => ({ name, paramsForName }));
   }, [params]);
+  const fused: Fuse.FuseResult<any>[] = useMemo(() => {
+    if (search.trim().length === 0) return sortedParams.map((item) => ({ item, matches: [], refIndex: -1 }));
+    return new Fuse(sortedParams, { keys: ['name'], includeMatches: true }).search(search);
+  }, [sortedParams, search]);
 
   return (
-    <table className="wt-table wt-table_wide wt-table_size_xs">
-      <thead className="wt-text-3">
-        <tr>
-          <th>Name</th>
-          {prefixes.map((prefix) => (
-            <th key={prefix}>{prefix}</th>
-          ))}
-        </tr>
-      </thead>
-      <tbody className="wt-text-3">
-        {map.map(([name, paramsForName]) => {
-          return (
-            <tr>
-              <td width={400}>{name}</td>
-              <PrefixRow paramsForName={paramsForName} />
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+    <>
+      <Row>
+        <Col span={4}>
+          <Input size="xs" placeholder="Search by param name" onChange={(e) => setSearch(e.target.value)} />
+        </Col>
+      </Row>
+      <table className="wt-table wt-table_wide wt-table_size_xs wt-offset-top-12">
+        <thead className="wt-text-3">
+          <tr>
+            <th>Name</th>
+            {prefixes.map((prefix) => (
+              <th key={prefix}>{prefix}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody className="wt-text-3">
+          {fused.map(({ item: { name, paramsForName }, matches }) => {
+            const searchWords = matches?.flatMap(({ indices }) => indices.map((tuple) => name.slice(tuple[0], tuple[1] + 1))) ?? [];
+            return (
+              <tr>
+                <td width={400}>
+                  <Highlighter searchWords={searchWords} textToHighlight={name} />
+                </td>
+                <PrefixRow paramsForName={paramsForName} />
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
   );
 };
